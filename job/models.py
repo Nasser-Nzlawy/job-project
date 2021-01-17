113cc2b2a51bec2c74acdad41c0af9db7be6b9ef
Nasser-Nzlawy <nassersaleh516@gmail.com>
2021-01-17 12:19:32 +0200

    init

--- job/models.py
+++ job/models.py
@@ -19,6 +19,9 @@ class Job(models.Model):
     Experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
     image = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self,*args)
 
     def __str__(self):
         return self.title
