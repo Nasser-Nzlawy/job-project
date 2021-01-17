91d640c1a2fcbaab3b784b12f7524d742d679cf5
Nasser Nzlawy <nassersaleh516@gmail.com>
2021-01-17 03:04:00 +0200

    first commit

---
+++ job/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+# Create your models here.
+JOB_TYPE =(
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
+def image_upload(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE) 
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    Salary = models.IntegerField(default=0)
+    Experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+    
