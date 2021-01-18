d84688ca52749fed77e93debab3148a77df91098
Nasser-Nzlawy <nassersaleh516@gmail.com>
2021-01-18 15:47:29 +0200

    init

--- job/models.py
+++ job/models.py
@@ -45,6 +45,7 @@ class Apply(models.Model):
     website = models.URLField()
     cv = models.FileField(upload_to='apply')
     cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Job
 from django.core.paginator import Paginator
+from  .models import apply_form
 # Create your views here.
 
 def job_list(request):
